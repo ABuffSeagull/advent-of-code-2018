547912b40bbcf21e0d42cd79f380757401db3520
ABuffSeagull <reecevanatta@gmail.com>
2018-12-03 17:03:17 -0500

    Finished part 1 in clojure

--- day1/clj/src/day1/core.clj
+++ day1/clj/src/day1/core.clj
@@ -1,7 +1,12 @@
-(ns day1.core
-  (:gen-class))
+(ns	day1.core
+	(:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn	-main
+	"Day 1!"
+	[filename]
+	(println
+	 	(reduce +
+		 	(map
+			 	#(Integer/parseInt %)
+				(clojure.string/split-lines
+					(slurp filename))))))
