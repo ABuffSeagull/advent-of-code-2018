0f52370ee864f18d91a836ec9d16b4a85794bf76
ABuffSeagull <reecevanatta@gmail.com>
2018-12-03 16:22:47 -0500

    Generate the correct template

--- day1/clj/test/day1/core_test.clj
+++ day1/clj/test/day1/core_test.clj
@@ -1,6 +1,6 @@
-(ns clj.core-test
+(ns day1.core-test
   (:require [clojure.test :refer :all]
-            [clj.core :refer :all]))
+            [day1.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
