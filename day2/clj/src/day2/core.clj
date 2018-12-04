0a1aefeb915a6a8dd4a3704b103fa42d12e78c51
ABuffSeagull <reecevanatta@gmail.com>
2018-12-03 22:07:40 -0500

    Finished day 2, part 1

--- day2/clj/src/day2/core.clj
+++ day2/clj/src/day2/core.clj
@@ -1,7 +1,22 @@
 (ns day2.core
+	(:require clojure.set clojure.string)
   (:gen-class))
 
+(defn filter-and-count-by-freq
+	[n coll]
+	(->> coll
+			 (map frequencies)
+			 (map clojure.set/map-invert)
+			 (filter #(contains? % n))
+			 (count)))
+
+(defn part-one
+	[coll]
+	(* (filter-and-count-by-freq 2 coll) (filter-and-count-by-freq 3 coll)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Day 2"
+  [filename]
+  (println
+	 (part-one
+		(clojure.string/split-lines (slurp filename)))))
