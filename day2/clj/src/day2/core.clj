bd28c8a8fa39e5966a10c6588663a24ae63fc099
ABuffSeagull <reecevanatta@gmail.com>
2018-12-03 23:51:26 -0500

    Finished day 2, part 2

--- day2/clj/src/day2/core.clj
+++ day2/clj/src/day2/core.clj
@@ -14,9 +14,32 @@
 	[coll]
 	(* (filter-and-count-by-freq 2 coll) (filter-and-count-by-freq 3 coll)))
 
+(defn get-diffs-in-strs
+	[str1 str2]
+	(->>
+	 	(map #(vector %1 %2) str1 str2)
+	 	(filter #(not= (first %) (second %)))
+		count))
+
+(defn get-same-letters
+	[str1 str2]
+	(->>
+	 	(map #(vector %1 %2) str1 str2)
+	 	(filter #(= (first %) (second %)))
+		(map first)
+	 	(clojure.string/join)))
+
+(defn part-two
+	[[head & tail]]
+	(let [coll (map #(get-diffs-in-strs head %) tail)
+				found (.indexOf coll 1)]
+		(if (not= found -1)
+			(get-same-letters head (nth tail found))
+			(recur tail))))
+
 (defn -main
   "Day 2"
   [filename]
   (println
-	 (part-one
+	 (part-two
 		(clojure.string/split-lines (slurp filename)))))
